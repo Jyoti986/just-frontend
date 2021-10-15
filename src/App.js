e87b6c4b38a665c8c5b7319cfa9f48087c12bc42
SobhanDash <sobhan7dash@gmail.com>
2021-10-15 18:46:51 +0530

    Sobhan: Basic folder structure created

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import "../node_modules/bootstrap/dist/css";
-import "../node_modules/bootstrap/dist/js";
-
-function App() {
-  return <div className="App"></div>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Hello World</h3>
+    </div>
+  );
+}
+
+export default App;
