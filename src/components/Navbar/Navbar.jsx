bef02213500c015579c4f2658df866a1b3650c40
TrilochanSahoo <trilochansahoo026@gmail.com>
2021-10-16 23:33:54 +0530

    update : add styling nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import "./Navbar.css"
+
+const element = <FontAwesomeIcon icon={faSearch} />
 
 const Navbar = ()=>{
     return (
@@ -8,11 +13,21 @@ const Navbar = ()=>{
                     <h2>Logo</h2>
                 </div>
                 <div className="search_bar">
-                    <input type="text" name="" id="" />
-                    <button>search</button>
+                    <input type="search_text" name="" id="" placeholder="Enter search item"/>
+                    <a className="search_btn" href="#">
+                        {element}
+                    </a>
                 </div>
-                <button className="create">create</button>
-                <div className="message">message</div>
+                <div className="create">
+                    <button className="create_btn">create</button>
+                </div>
+                <div className="message">
+                    <ul>
+                        <li>list1</li>
+                        <li>list1</li>
+                        <li>list1</li>
+                    </ul>
+                </div>
             </nav>
         </>
     )
