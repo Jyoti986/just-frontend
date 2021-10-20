114bbec547448648a81e1bd5b9d708b69012d04f
SobhanDash <sobhan7dash@gmail.com>
2021-10-20 16:31:42 +0530

    Feat: Sobhan- Added Basic Routing

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import "./styles/App.css";
 // import "../node_modules/bootstrap/dist/css/bootstrap.css";
 // import "../node_modules/bootstrap/dist/js/bootstrap";
 import RouteConfig from "./Router";
-// import Index from "./pages/Index/Index"
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 function App() {
   return <RouteConfig />;
